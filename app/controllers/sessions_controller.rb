2bb6bd4965c8d2a19d8a94b9a16004daf2060ac2
Matt Farmer <mrfarmer777@gmail.com>
2018-07-31 03:19:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   def create
     resp=Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params["client_id"]=ENV['FOURSQUARE_CLIENT_ID']
-      req.params
+      req.params["client_secret"]=ENV["FOURSQUARE_SECRET"]
+      req.params["grant_type"]="authorization_code"
 
 end
